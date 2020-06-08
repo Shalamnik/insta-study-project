ac32ff8aa5a4e76696ad6cc752b639309dbaeb54
shalamnik <shalamberidze00@bk.ru>
2020-06-08 16:34:02 +0300

    added script for main__navbar

--- js/focus-search.js
+++ js/focus-search.js
@@ -1,12 +1,15 @@
+// get elements
 let inputEl = document.querySelector('.header__search input');
 let inputSearchImg = document.querySelector('.search-img');
 let inputQuitImg = document.querySelector('.quit-img');
 
+//add event to clicked search input
 inputEl.addEventListener('focus', () => {
     inputSearchImg.style.left = '5px';
     inputQuitImg.hidden = false;
 })
 
+// add event to unclicked search input
 inputEl.addEventListener('blur', () => {
     inputSearchImg.style.left = '72px';
     inputQuitImg.hidden = true;
